0268e53f34578e8cdf7dbdd8015c7ca4b00c5993
Анна Бородачева <anuta.gulik@gmail.com>
2020-09-22 00:25:11 +0300

    added solution for the first task

--- classify double/solution.c
+++ classify double/solution.c
@@ -10,61 +10,110 @@
  * You should use them in the main sections.
  */
 
-uint64_t convertToUint64 (double number) {
-    return *((uint64_t *)(&number));
+uint64_t convertToUint64(double number) {
+    return *((uint64_t*)(&number));
 }
 
-bool getBit (const uint64_t number, const uint8_t index) {
-    /// Your code here...
+bool getBit(const uint64_t number, const uint8_t index) {
+    uint64_t mask = 1 << index;
+    return (mask & number) >> index;
 }
 
+bool isNegative(uint64_t number) {
+    return getBit(number, 63);
+}
 
+bool isDenormalized(uint64_t number) {
+    for (uint8_t i = 62; i > 51; --i) {
+        if (getBit(number, i)) {
+            return false;
+        }
+    }
+    for (uint8_t i = 51; i > 0; --i) {
+        if (getBit(number, i)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isExponentAllOne(uint64_t number) {
+    for (uint8_t i = 62; i > 51; --i) {
+        if (!getBit(number, i)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isNormalized(uint64_t number) {
+    bool zeroFlag = false;
+    bool oneFlag = false;
+    for (uint8_t i = 52; i < 63; ++i) {
+        if (getBit(number, i)) {
+            oneFlag = true;
+        }
+        else {
+            zeroFlag = true;
+        }
+    }
+    return oneFlag & zeroFlag;
+}
 /**
  * Checkers here:
  */
 
-bool checkForPlusZero (uint64_t number) {
-    /// Your code here.
+bool checkForPlusZero(uint64_t number) {
+    return number == 0x0000000000000000;
 }
 
-bool checkForMinusZero (uint64_t number) {
+bool checkForMinusZero(uint64_t number) {
     return number == 0x8000000000000000;
 }
 
-bool checkForPlusInf (uint64_t number) {
-    /// Your code here.
+bool checkForPlusInf(uint64_t number) {
+    return number == 0x7FE0000000000000;
 }
 
-bool checkForMinusInf (uint64_t number) {
-    /// Your code here.
+bool checkForMinusInf(uint64_t number) {
+    return number == 0xFFE0000000000000;
 }
 
-bool checkForPlusNormal (uint64_t number) {
-    /// Your code here.
+bool checkForPlusNormal(uint64_t number) {
+    return !isNegative(number) & isNormalized(number);
 }
 
-bool checkForMinusNormal (uint64_t number) {
-    /// Your code here.
+bool checkForMinusNormal(uint64_t number) {
+    return isNegative(number) & isNormalized(number);
 }
 
-bool checkForPlusDenormal (uint64_t number) {
-    /// Your code here.
+bool checkForPlusDenormal(uint64_t number) {
+    return !isNegative(number) & isDenormalized(number);
 }
 
-bool checkForMinusDenormal (uint64_t number) {
-    /// Your code here.
+bool checkForMinusDenormal(uint64_t number) {
+    return isNegative(number) & isDenormalized(number);
 }
 
-bool checkForSignalingNan (uint64_t number) {
-    /// Your code here.
+bool checkForSignalingNan(uint64_t number) {
+    bool oneFlag = false;
+    for (uint8_t i = 0; i <= 50; ++i) {
+        if (getBit(number, i)) {
+            oneFlag = true;
+        }
+    }
+    return isExponentAllOne(number) & (!getBit(number, 51)) & oneFlag;
 }
 
-bool checkForQuietNan (uint64_t number) {
-    /// Your code here.
+bool checkForQuietNan(uint64_t number) {
+    return isExponentAllOne(number) & getBit(number, 51);
 }
 
 
-void classify (double number) {
+void classify(double number) {
+
+    printf("%u\n", convertToUint64(number));
+
     if (checkForPlusZero(convertToUint64(number))) {
         printf("Plus zero\n");
     }
